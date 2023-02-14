c55a9d1a25a67444853a1e667cca0126997ad25e
kyounggseo <pokj930@naver.com>
2023-02-14 21:29:48 +0900

    first commit

---
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -0,0 +1,31 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+    @GetMapping("/fragment")
+    public String template() {
+        return "template/fragment/fragmentMain";
+    }
+
+    /**
+     * 템플릿 레이아웃
+     */
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+
+    /**
+     * 템플릿 레이아웃 확장
+     */
+    @GetMapping("/layoutExtend")
+    public String layoutExtends() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
+}
